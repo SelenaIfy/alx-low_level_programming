0c5cdbfcb604610ccd03cf02f47e1c4f8f3293dc
SelenaIfy <princess01selena@gmail.com>
2023-06-21 13:07:08 -0700

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints multiple of 3 5 under 1024
+ * Return: always 0
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
