7badc7dede21ac0f495a3eb3142789fc8b1f0330
SelenaIfy <princess01selena@gmail.com>
2023-06-27 19:49:25 -0700

    print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - reverse
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int leng = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		leng++;
+		s++;
+	}
+	s--;
+	for (o = leng; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
