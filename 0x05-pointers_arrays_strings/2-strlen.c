3ddc729194f44e7d75ad07eb78367cee04155d18
SelenaIfy <princess01selena@gmail.com>
2023-06-27 19:36:47 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - returns length of string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int leng = 0;
+
+	while (*s != '\0')
+	{
+		leng++;
+		s++;
+	}
+
+	return (leng);
+}
