a471558d6d27635ebbafbb9489599ade7e507dcc
SelenaIfy <princesss01selena@gmail.com>
2023-08-09 13:33:06 -0700

    static library

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,13 +1,12 @@
 #include "main.h"
-void _puts(char *str)
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- * main - Entry point
+ * _puts - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void _puts(char *s)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
 }
